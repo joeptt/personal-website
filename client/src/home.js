61d42fe97787198d6067678581107ff03018912e
Joe Petereit <joe.petereit@web.de>
2022-07-13 17:52:48 +0200

    first layouts

--- client/src/home.js
+++ client/src/home.js
@@ -1,28 +1,50 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-    let [positionX, setPositionX] = useState(100);
-    const speed = 100;
+    let [positionX, setPositionX] = useState(10);
+    let [backgroundPositionX, setBackgroundPositionX] = useState(0);
+    const speed = 5;
+    const backGroundSpeed = 20;
 
     useEffect(() => {
         console.log("mounted");
+        setBackgroundPositionX(0);
         window.addEventListener("keydown", (event) => {
-            moveUser(event);
+            moveUser(event.keyCode);
         });
     }, []);
 
-    const moveUser = (event) => {
-        if (event.keyCode === 39) {
-            console.log("tip Top klappt");
+    const moveUser = (key) => {
+        if (key === 39) {
+            if (positionX >= 50) {
+                setBackgroundPositionX(
+                    (backgroundPositionX =
+                        backgroundPositionX - backGroundSpeed)
+                );
+                return;
+            }
             setPositionX((positionX = positionX + speed));
+        } else if (key === 37) {
+            if (positionX <= 10) {
+                return;
+            }
+            setPositionX((positionX = positionX - speed));
         }
     };
 
     return (
-        <div className="home-wrapper">
-            <div style={{ left: `${positionX}px` }} className="player-wrapper">
+        <>
+            <div
+                style={{ left: `${backgroundPositionX}px` }}
+                className="home-wrapper"
+            >
+                <div style={{ backgroundColor: "green" }}></div>
+                <div style={{ backgroundColor: "yellow" }}></div>
+                <div style={{ backgroundColor: "blue" }}></div>
+            </div>
+            <div style={{ left: `${positionX}%` }} className="player-wrapper">
                 PLAYER
             </div>
-        </div>
+        </>
     );
 }
