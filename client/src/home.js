72a1a0a6a573976ee056b3bb813dc4d99fccf994
Joe Petereit <joe.petereit@web.de>
2022-07-14 13:10:02 +0200

    added overlapping detection function

--- client/src/home.js
+++ client/src/home.js
@@ -1,18 +1,34 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function Home() {
     let [positionX, setPositionX] = useState(10);
     let [backgroundPositionX, setBackgroundPositionX] = useState(0);
+    let [test, setTest] = useState("hidden");
+    const refUser = useRef(null);
+    const refHouse = useRef(null);
     const speed = 5;
     const backGroundSpeed = 20;
 
     useEffect(() => {
-        console.log("mounted");
-        setBackgroundPositionX(0);
         window.addEventListener("keydown", (event) => {
             moveUser(event.keyCode);
         });
     }, []);
+
+    useEffect(() => {
+        checkForBuilding();
+    }, [positionX, backgroundPositionX]);
+
+    const checkForBuilding = () => {
+        const userX = refUser.current.getBoundingClientRect();
+        const houseX = refHouse.current.getBoundingClientRect();
+        if (userX.x - houseX.x > -200 && userX.x - houseX.x < 500) {
+            console.log("Enter Projects House");
+            setTest("visible");
+            return;
+        }
+        setTest("hidden");
+    };
 
     const moveUser = (key) => {
         if (key === 39) {
@@ -26,6 +42,12 @@ export default function Home() {
             setPositionX((positionX = positionX + speed));
         } else if (key === 37) {
             if (positionX <= 10) {
+                if (backgroundPositionX < 0) {
+                    setBackgroundPositionX(
+                        (backgroundPositionX =
+                            backgroundPositionX + backGroundSpeed)
+                    );
+                }
                 return;
             }
             setPositionX((positionX = positionX - speed));
@@ -38,11 +60,30 @@ export default function Home() {
                 style={{ left: `${backgroundPositionX}px` }}
                 className="home-wrapper"
             >
-                <div style={{ backgroundColor: "green" }}></div>
-                <div style={{ backgroundColor: "yellow" }}></div>
-                <div style={{ backgroundColor: "blue" }}></div>
+                <div
+                    className="pages"
+                    style={{
+                        backgroundColor: "green",
+                    }}
+                ></div>
+                <div className="pages" style={{ backgroundColor: "yellow" }}>
+                    <div ref={refHouse} className="house">
+                        <div
+                            className="test"
+                            style={{ visibility: test }}
+                        ></div>
+                    </div>
+                </div>
+                <div
+                    className="pages"
+                    style={{ backgroundColor: "blue" }}
+                ></div>
             </div>
-            <div style={{ left: `${positionX}%` }} className="player-wrapper">
+            <div
+                ref={refUser}
+                style={{ left: `${positionX}%` }}
+                className="player-wrapper"
+            >
                 PLAYER
             </div>
         </>
